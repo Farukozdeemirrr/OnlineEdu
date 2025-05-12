e7fe3b6997e92947d2b3c4c0857fe8a8ec67fe3c
Farukozdeemirrr <farukozdeemirrr@gmail.com>
2025-05-12 13:06:56 +0300

    Added | SocaiMedia, Message

---
+++ OnlineEdu.API/Controllers/MessageController.cs
@@ -0,0 +1,61 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql.Replication.PgOutput.Messages;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.DTO.DTOs.CourseDtos;
+using OnlineEdu.DTO.DTOs.MessageDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MessageController : ControllerBase
+    {
+       private readonly IMapper _mapper;
+       private readonly IGenericService<Message> _messageService;
+        public MessageController(IMapper mapper, IGenericService<Message> messageService)
+        {
+            _mapper = mapper;
+            _messageService = messageService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllList()
+        {
+            var values = _messageService.TGetAllList();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _messageService.TGetBydId(id);
+            return Ok(value);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteById(int id)
+        {
+            //Id 0 İse, bu Id li değer bulunamadı hatası döndürme işlemi eklenecek(Backend tarafında)
+            _messageService.TDelete(id);
+            return Ok("Mesaj alanı silindi");
+        }
+        [HttpPost]
+        public IActionResult Create(CreateMessageDto createMessageDto)
+        {
+            var newValue = _mapper.Map<Message>(createMessageDto);
+            _messageService.TAdd(newValue);
+            return Ok("Yeni Mesaj alanı oluşturuldu");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateMessageDto updateMessageDto)
+        {
+            var value = _mapper.Map<Message>(updateMessageDto);
+            _messageService.TUpdate(value);
+            return Ok("Mesaj alanı güncellendi");
+        }
+
+    }
+
+}
---
+++ OnlineEdu.API/Controllers/SocailMediaController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineEdu.Business.Abstract;
+using OnlineEdu.DTO.DTOs.MessageDtos;
+using OnlineEdu.DTO.DTOs.SocialMediaDtos;
+using OnlineEdu.Entity.Entities;
+
+namespace OnlineEdu.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SocailMediaController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IGenericService<SocialMedia> _socialMediaService;
+        public SocailMediaController(IMapper mapper, IGenericService<SocialMedia> socialMediaService)
+        {
+            _mapper = mapper;
+            _socialMediaService = socialMediaService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllList()
+        {
+            var values = _socialMediaService.TGetAllList();
+            return Ok(values);
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var value = _socialMediaService.TGetBydId(id);
+            return Ok(value);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteById(int id)
+        {
+            //Id 0 İse, bu Id li değer bulunamadı hatası döndürme işlemi eklenecek(Backend tarafında)
+            _socialMediaService.TDelete(id);
+            return Ok("Sosyal Medya alanı silindi");
+        }
+        [HttpPost]
+        public IActionResult Create(CreateSocialMediaDto createSocialMediaDto)
+        {
+            var newValue = _mapper.Map<SocialMedia>(createSocialMediaDto);
+            _socialMediaService.TAdd(newValue);
+            return Ok("Sosyal Medya alanı oluşturuldu");
+        }
+
+        [HttpPut]
+        public IActionResult Update(UpdateSocialMediaDto updateSocialMediaDto)
+        {
+            var value = _mapper.Map<SocialMedia>(updateSocialMediaDto);
+            _socialMediaService.TUpdate(value);
+            return Ok("Sosyal Medya güncellendi");
+        }
+    }
+}
