3c7629165767fffe5469a31eba663b30230546e3
Farukozdeemirrr <farukozdeemirrr@gmail.com>
2025-05-12 14:31:49 +0300

    Fixed, Added UI File

--- OnlineEdu.WebUI/Models/ErrorViewModel.fs
+++
@@ -1,9 +0,0 @@
-namespace OnlineEdu.WebUI.Models
-
-open System
-
-type ErrorViewModel =
-    { RequestId: string }
-
-    member this.ShowRequestId =
-        not (String.IsNullOrEmpty(this.RequestId))
